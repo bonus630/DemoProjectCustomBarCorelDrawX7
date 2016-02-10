3ef1348967068c70a40e2ca4b618f2bda8063f33
bonus630 <bonus630@gmail.com>
2016-02-10 11:50:33 -0200

    Adicionando desenho para ponta de flecha em uma linha, e calculo de área para uma curva fechada

--- DemoProjectCustomBarCorelDrawX7/BasicDraw.xaml.cs
+++ DemoProjectCustomBarCorelDrawX7/BasicDraw.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System.Windows.Controls;
-
+using Corel.Interop.VGCore;
 namespace br.corp.bonus630.DemoProjectCustomBarCorelDrawX7
 {
     /// <summary>
@@ -14,6 +14,25 @@ namespace br.corp.bonus630.DemoProjectCustomBarCorelDrawX7
             btn_drawSquad.Click += (s, e) => { app.ActiveDocument.ActiveLayer.CreateRectangle(0, 0, 1.5, 1.5); };
             btn_drawLine.Click += (s, e) => { app.ActiveDocument.ActiveLayer.CreateLineSegment(0,0,1.5,0); };
             btn_drawText.Click += (s, e) => { app.ActiveDocument.ActiveLayer.CreateArtisticText(0,0,"Hello, this is a test"); };
+            btn_drawArrow.Click += (s, e) => { DrawArrow(app); };
+            btn_curveArea.Click += (s, e) => { CurveArea(app); };
+            
+        }
+        private void CurveArea(Corel.Interop.VGCore.Application app)
+        {
+            //Calcula área de um shape selecionado
+            //tratamento de erro é necessário para evitar que o CorelDraw feche, exemplo: nenhum objeto selecionado
+            global::System.Windows.MessageBox.Show(string.Format("Área: {0}",app.ActiveShape.Curve.Area));
+        }
+        public void DrawArrow(Corel.Interop.VGCore.Application app)
+        {
+            
+           //Criando um segmento de linha com ponta de flecha
+            Corel.Interop.VGCore.Shape line = app.ActiveDocument.ActiveLayer.CreateLineSegment(0, 0, 1.5, 0);
+            line.Outline.EndArrow = app.ArrowHeads[2];
+            //Inicio de flecha
+            // line.Outline.StartArrow = app.ArrowHeads[2];
+      
         }
     }
 }
